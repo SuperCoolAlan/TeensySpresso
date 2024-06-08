0f672c95fde23ead9af69921aa3c64dc7ba57b1f
Alan Sandoval <asandov@asandov.com>
2024-06-08 16:35:28 -0400

    break out functions

--- teensy32_homebrew/teensy32_homebrew.ino
+++ teensy32_homebrew/teensy32_homebrew.ino
@@ -1,15 +1,18 @@
 // Rancilio Silvia PID controller
 // Alan Sandoval
-// September 2022
+// June 2024
 
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
-#include <PID_v1.h>
+#include <PID_v1_bc.h>
 
 #define CS 10
 #define SCLK 13
 #define DIN 12
+
 #define RelayPin 6
+#define RedLEDOne 23
+#define RedLEDTwo 20 
 
 Adafruit_MAX31855 tc0(SCLK, CS, DIN);
 
@@ -23,38 +26,50 @@ int WindowSize = 1000;
 unsigned long windowStartTime;
 
 void setup() {
-  // debug log
+  // create serial
   Serial.begin(9600);
   long unsigned debug_start = millis ();
-  while (!Serial && ((millis () - debug_start) <= 5000)); 
-  
+  while (!Serial && ((millis () - debug_start) <= 2000)); 
+
+  // configure SPI outputs
   pinMode(CS, OUTPUT);
   digitalWrite(CS, HIGH);
 
-  pinMode(RelayPin, OUTPUT);
-
-  windowStartTime = millis();
-
-  //initialize the variables we're linked to
-  Setpoint = 28;
-
-  //tell the PID to range between 0 and the full window size
-  myPID.SetOutputLimits(0, WindowSize);
-
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
+  setupPid();
   
   delay(500);
 }
 
+void setupPid() {
+    pinMode(RelayPin, OUTPUT);
+    windowStartTime = millis();
+  
+    //initialize the variables we're linked to
+    Setpoint = 28;
+  
+    //tell the PID to range between 0 and the full window size
+    myPID.SetOutputLimits(0, WindowSize);
+  
+    //turn the PID on
+    myPID.SetMode(AUTOMATIC);
+}
+
 void loop() {
   double Input = tc0.readCelsius();
-  if(isnan(Input)){
-    Serial.print("MAX31855 error code: ");
+
+  calculatePid(Input);
+
+  delay(500);
+}
+
+void calculatePid(double temp) {
+  if(isnan(temp)){
+    Serial.print("MAX31855 error: ");
     Serial.println(tc0.readError());
   }else{
-    Serial.print("Temperature is "); 
-    Serial.println(Input);
+    Serial.print("Current temperature: "); 
+    Serial.println(temp);
+    
     myPID.Compute();
 
     unsigned long now = millis();
@@ -62,10 +77,16 @@ void loop() {
     { //time to shift the Relay Window
       windowStartTime += WindowSize;
     }
-    if (Output > now - windowStartTime) digitalWrite(RelayPin, HIGH);
-    else digitalWrite(RelayPin, LOW);
-    Serial.print("Relay mode is: ");
+    if (Output > now - windowStartTime) {
+      digitalWrite(RelayPin, HIGH);
+      digitalWrite(RedLEDOne, HIGH);
+    }
+    else {
+      digitalWrite(RelayPin, LOW);
+      digitalWrite(RedLEDOne, LOW);
+    }
+    Serial.print("Relay: ");
     Serial.println(digitalRead(RelayPin));
   }
-  delay(500);
+  
 }
